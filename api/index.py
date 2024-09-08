7c29927d936511563f5a0cb3cadaa431b58026cf
yagnesh-3 <162590670+yagnesh-3@users.noreply.github.com>
2024-09-08 15:58:56 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello, World!'
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    """
+    API endpoint to get user details from LPU Live.
+    """
+    id = "12223854"
+    token = "9daacfb1e97a628660431de6c9442481"
+    url = f"https://lpulive.lpu.in/fugu-api/api/chat/groupChatSearch?en_user_id={token}&search_text={id}&user_role=USER"
+    return {"id" : id}
 
 @app.route('/about')
 def about():
